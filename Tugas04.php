7d4b2ecc9cfbe1793c0f75cfbf8c25d729d89cb3
Syifahzn <101611794+Syifahzn@users.noreply.github.com>
2022-03-15 10:37:31 +0800

    Add files via upload

---
+++ Tugas04.php
@@ -0,0 +1,14 @@
+<?php
+$nilai = 80;
+if ($nilai >= 80){
+    echo "A \n";
+} else ($nilai >= 70 && $nilai <= 79){
+    echo "B \n";
+} else ($nilai >= 60 && $nilai <= 69){
+    echo "C \n";
+} else ($nilai >= 50 && $nilai <= 59){
+    echo "D \n";
+} else ($nilai <= 50){
+    echo "E \n";
+}
+?>
